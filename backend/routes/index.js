a4ea73f74f34df212508bf45e32667b2bc06d53d
Michael Gann <michael.gann@live.com>
2021-05-06 11:04:59 -0600

    server and db setup complete

---
+++ backend/routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+
+// Add a XSRF-TOKEN cookie in development
+// if (process.env.NODE_ENV !== "production") {
+//     router.get("/api/csrf/restore", (req, res) => {
+//       res.cookie("XSRF-TOKEN", req.csrfToken());
+//       res.status(201).json({});
+//     });
+//   }
+
+module.exports = router;
