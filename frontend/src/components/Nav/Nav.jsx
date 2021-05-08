3daa17843704eb372fcf8690056359653311dc7f
Michael Gann <michael.gann@live.com>
2021-05-07 18:47:27 -0600

    add fonts

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ import "./nav.css"
 const Nav = () => {
     return (
         <nav>
-            <h1>Todo-App</h1>
+            <h1>Todo's</h1>
             <div className="login-button-container">
                 <button className="login-button">Login</button>
             </div>
