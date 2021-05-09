133f10b53459ae03a23c60f69108936e7a2dbd69
Michael Gann <michael.gann@live.com>
2021-05-09 09:48:56 -0600

    update app.js to serve favicon

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.static("/frontend/public"));
-// app.use(favicon(path.join(__dirname, "..", "/frontend/public", "favicon.ico")));
+app.use(favicon(path.join(__dirname, "..", "/frontend/public", "favicon.ico")));
 
 if (!isProduction) {
   // enable cors only in development
