1e42629d96af8f36d308ca7214fce2db367186f9
Michael Gann <michael.gann@live.com>
2021-05-09 20:56:42 -0600

    update splash styling

--- frontend/src/components/Splash/Splash.jsx
+++ frontend/src/components/Splash/Splash.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import "./splash.css"
+import "./splash.css";
 
 const Splash = () => {
-    return (
-        <div className="splash-container">
-            <div className="splash-content">
-                <h2 className="splash-welcome">
-                    Welcome!
-                </h2>
-                <p className="splash-text">Thank you for the opportunity to create this app!</p>
-                <p className="splash-text">I hope you enjoy!</p>
-            </div>
-
+  return (
+    <main>
+      <div className="splash-container">
+        <div className="splash-content">
+          <h2 className="splash-welcome">Welcome to my full stack todo app!</h2>
+          <p className="splash-text">
+            Thank you for the opportunity to create this app.
+          </p>
+          <p className="splash-text">I hope you enjoy!</p>
+          <p className="splash-text copy">&copy; Michael Gann</p>
         </div>
+      </div>
+    </main>
+  );
+};
 
-    )
-}
-
-export default Splash
+export default Splash;
